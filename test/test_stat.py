95bec48f247c4e0648ffff79f566f41839c3d149
carvilgar1 <villa.carlosjesus@gmail.com>
2025-12-30 20:42:16 +0100

    test: Add unit tests for Stat class modifiers

---
+++ test/test_stat.py
@@ -0,0 +1,21 @@
+from src.Stats import Stat
+
+
+def test_stat_modifiers_sequence():
+    st = Stat(18)
+    assert st.mod == 4
+    st.addMiscModifier(1)
+    assert st.score == 19
+    assert st.mod == 4
+    st.addTmpModifier(4)
+    st.addTmpModifier(-2)
+    assert st.score == 21
+    assert st.mod == 5
+    st.addTmpModifier(-2)
+    assert st.score == 19
+    assert st.mod == 4
+    st.addTmpModifier(-4)
+    assert st.score == 15
+    assert st.mod == 2
+    assert st.misc == 1
+    assert st.tpm == -4
